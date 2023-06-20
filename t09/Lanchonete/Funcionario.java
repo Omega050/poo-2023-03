f8806c686e65ad8eef7fd5fddbd826b48a22f7dc
BrianMath <brian@discente.ufg.br>
2023-06-19 21:43:05 -0300

    Teste

---
+++ t09/Lanchonete/Funcionario.java
@@ -0,0 +1,13 @@
+package t09.Lanchonete;
+
+public class Funcionario {
+    private String name;
+
+    public Funcionario(String name) {
+        this.name = name;
+    }
+
+    public String getEmpName() {
+        return name;
+    }
+}
